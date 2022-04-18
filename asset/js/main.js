3e406367600964f3720e022004e27c9bb01beff6
najmul <najmulcse2@gmail.com>
2022-04-18 22:16:01 +0600

    wedding project design

---
+++ asset/js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+    "use strict";
+
+    // $(window).on('scroll', function () {
+    //     var scroll = $(window).scrollTop();
+    //     if (scroll < 100) {
+    //         $(".header-sticky").removeClass("sticky");
+    //     } else {
+    //         $(".header-sticky").addClass("sticky");
+    //     }
+    // });
+
+
+
+
+
+})(jQuery);
